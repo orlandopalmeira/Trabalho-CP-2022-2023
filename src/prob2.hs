893dd575ec307ad10af5dbb837374a527f6d56c0
orlandopalmeira <orlandopalmeira51@gmail.com>
2022-12-13 22:43:34 +0000

    Problema 2 resolvido

--- src/prob2.hs
+++ src/prob2.hs
@@ -6,6 +6,7 @@ import Cp2223data
 
 test  = take 10 acm_ccs 
 test2 = take 30 acm_ccs  
+
 -- Parte 1
 out :: [String] -> Either String (String,[String])
 out [s] = i1 s
@@ -27,12 +28,21 @@ tax :: [String] -> Exp String String
 tax = anaExp (gene)
 
 -- Parte 2
+{-
+Versão antiga mais feia mas possivelmente mais fácil de explicar
 post :: Exp String String -> [[String]] -- incompleta
-post tr = aux (tr)
+post tr = (tail.rr.concat.map prefixes) (aux tr)
     where
+        -- fazer os prefixos dos caminhos mais longos e eliminar os elementos repetidos no resultado final.
+        rr = foldr (\x acc -> x : filter (/= x) acc) [] -- remove os repetidos de uma lista
         aux (Var x) = [[x]]
         aux (Term x []) = [[x]]
-        aux (Term x l) = map (x:) (concat(map aux l))
+        aux (Term x l) = map (x:) (concat(map aux l)) -}
+
+post :: Exp String String -> [[String]]
+post (Var v) = [[v]]
+post (Term v []) = [[v]]
+post (Term x children) = [x]:concatMap (\y -> map (x:) (post y)) children
 
 
 tudo ::[String] -> [[String]]
